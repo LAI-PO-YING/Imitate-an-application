daac43ebb1580882b1d9a26864d40c3143c654c0
Ivan Lai <229705jack2gmail.com>
2021-03-12 11:36:24 +0800

    Initial Commit

---
+++ Imitate an application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Imitate an application
+//
+//  Created by Lai Po Ying on 2021/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
